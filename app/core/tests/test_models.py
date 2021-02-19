0ec15fb60d8f884ac0914fd289cd908c200f8be6
jsjackson <jsjackson@example.com>
2021-02-19 21:36:19 +1100

    Add list ingredients feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,3 +51,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingrdient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
